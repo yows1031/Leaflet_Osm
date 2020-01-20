b898a864825fd35313d2ec7c263df166eeb984e2
菅原 洋介 <>
2020-01-20 16:58:22 +0900

    1/20 comit

---
+++ main.js
@@ -0,0 +1,48 @@
+'use strict'
+
+{
+    var map = L.map('map').setView([38.4316, 141.3094], 12);
+
+    var geojsonFeature = [];
+    var test_popupContents = ['イトナブ<br>職場', 'もりや<br>そば屋', '野田屋<br>弁当屋', 'ローソン<br>コンビニ'];
+    var lat = [38.4316, 38.4306, 38.4299, 38.4327];
+    var lon = [141.3094, 141.3076, 141.3070, 141.3087];
+
+    var customClassName = ['.btn_Itnav', '.btn_Moriya', '.btn_Nodaya', '.btn_Lowson'];
+
+    var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxZoom: 19
+        });
+tileLayer.addTo(map);
+
+for (var i = 0; i < test_popupContents.length; i++) {
+    geojsonFeature.push({
+        "type": "Feature",
+        "properties": {
+        "popupContent": test_popupContents[i],
+        "className": customClassName[i]
+    },
+    "geometry": {
+      "type": "Point",
+      "coordinates": [lon[i], lat[i]] 
+  },
+    });
+
+    // var addCustomClassName = customClassName[i];
+   
+
+}
+
+L.geoJson(geojsonFeature,
+    {
+        onEachFeature: function(feature, layer){
+            if(feature.properties && feature.properties.popupContent){
+                layer.bindPopup(feature.properties.popupContent);
+            }
+           
+        }
+    }).addTo(map)
+    $(geoJson.getContainer()).addClass('test');
+  
+}
